e647a62d2dfaf3585e7893a0835923921051337a
Glenn Danthi <gdanthi@gmail.com>
2011-11-20 00:03:36 -0700

    first commit

---
+++ MobileXenCenter/services/main.py
@@ -0,0 +1,27 @@
+'''
+Created on Nov 19, 2011
+@author: glenn
+'''
+from flask import Flask, request
+import json
+app = Flask(__name__)
+
+errorcodes = {1 :"Access denied", 2:"Server not found"}
+
+@app.route("/mobilexencenter/virtualmachines/", methods=['POST'])
+def getvmlist():
+    if request.method == 'POST':
+        serveraddress = request.form['address']
+        username = request.form['username']
+        password = request.form['password']
+        if serveraddress == "192.168.100.100":
+            if username == "ramayan" and password == "heyram":
+                vmlist = ["Virtual Machine 1","Virtual Machine 2", "Virtual Machine 3"]
+                return json.dumps(vmlist)
+            else :
+                return errorcodes[1]
+        else :
+            return errorcodes[2]
+if __name__ == "__main__":
+    app.debug = 'true'
+    app.run()
